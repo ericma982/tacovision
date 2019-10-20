239caec00f86739e73021533b39057a6a5d8ca34
Robert Kim <bobbykim1013@yahoo.com>
2019-10-20 01:55:37 -0500

    WE LIVE IN A TACO SOCIETY

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tacovision
 TAMU Datathon 2019
+
+run the 'taco_backend.py' function from within the /backend directory
--- backend/taco_backend.py
+++ backend/taco_backend.py
@@ -6,6 +6,7 @@ address = input("Please enter the central street address: ").split()
 city = input("Please enter the city: ")
 state = input("Please enter the state (2 letter format): ")
 postal = str(input("Please enter the postal code: "))
+desired_radius = float(input("Desired search radius in miles? "))
 
 import requests
 import json
@@ -24,4 +25,33 @@ j_file = j.json()
 if j_file.get('results'):
     location = j_file.get('results')[0].get('geometry').get('location')
     lat_long = (location.get('lat'), location.get('lng'))
-print(lat_long)
+
+from math import sin, cos, sqrt, atan2, radians
+
+# approximate radius of earth in km
+R = 3958.8
+
+lat1 = radians(lat_long[0])
+lon1 = radians(lat_long[1])
+in_radius_rows = []
+for index, row in tacos.iterrows():
+    lat2 = radians(row['latitude'])
+    lon2 = radians(row['longitude'])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    if distance < 0:
+        print("OH NO!!!!!")
+
+    if distance < desired_radius:
+        in_radius_rows.append((row, distance))
+
+in_radius_rows.sort(key=lambda tup: tup[1])
+for row, distance in in_radius_rows:
+    print(row['menus.name'], 'at', row['name'], 'in', row['city'] + ', ' + row['state'], distance, 'miles away')
